895d183b16cca88afa49d625767bfbd717e1df91
rakesh-choudhury <choudhury.rak@husky.neu.edu>
2020-08-22 09:02:40 -0400

    profile edits

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -10,4 +11,18 @@ class UserRegisterForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username','email','password1','password2']
+
+class UserUpdateForm(forms.ModelForm):
+	"""docstring for ClassName"""
+	email = forms.EmailField()
+
+	class Meta:
+		model = User
+		fields = ['username','email']
+		
+class ProfileUpdateForm(forms.ModelForm):
+	class Meta(object):
+		model = Profile
+		fields = ['image']
+			
 		
